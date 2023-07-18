b5db467bb5733c052ec4d5078fce69a602b5d823
yymin1022 <yymin1022@gmail.com>
2023-07-19 01:44:05 +0900

    Added Meter Activity

---
+++ app/src/main/java/com/yong/taximeter/MeterActivity.kt
@@ -0,0 +1,11 @@
+package com.yong.taximeter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MeterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meter)
+    }
+}
