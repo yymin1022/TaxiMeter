aa43cfde72a058f43e9db1fc342832ade626b80b
yymin1022 <yymin1022@gmail.com>
2023-07-19 01:55:46 +0900

    Update MainHomeFragment : Added Button for Open MeterActivity

--- app/src/main/java/com/yong/taximeter/MainHomeFragment.kt
+++ app/src/main/java/com/yong/taximeter/MainHomeFragment.kt
@@ -1,17 +1,22 @@
 package com.yong.taximeter
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class MainHomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_home, container, false)
+        val baseView = inflater.inflate(R.layout.fragment_main_home, container, false)
+        val btnOpenMeter = baseView.findViewById<Button>(R.id.btn_home_openmeter)
+        btnOpenMeter.setOnClickListener { startActivity(Intent(context, MeterActivity::class.java)) }
+
+        return baseView
     }
 }
