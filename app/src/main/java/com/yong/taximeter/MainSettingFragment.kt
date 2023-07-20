db545186bc87ead3a769ab5d3fbe13d30e1d6ba3
yymin1022 <yymin1022@gmail.com>
2023-07-21 00:27:24 +0900

    Update MainSettingFragment : Cleaned UP

--- app/src/main/java/com/yong/taximeter/MainSettingFragment.kt
+++ app/src/main/java/com/yong/taximeter/MainSettingFragment.kt
@@ -8,6 +8,10 @@ import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 
+const val PREF_KEY_LOCATION = "pref_location"
+const val PREF_KEY_THEME = "pref_theme"
+const val PREF_KEY_INFO_COST = "pref_info_cost"
+const val PREF_KEY_INFO_VERSION = "pref_info_version"
 class MainSettingFragment : PreferenceFragmentCompat() {
     private lateinit var pref: SharedPreferences
     private lateinit var arrLocationKey: Array<String>
@@ -32,30 +36,28 @@ class MainSettingFragment : PreferenceFragmentCompat() {
     private var prefListener = OnSharedPreferenceChangeListener {
         sharedPreferences, key ->
             when(key) {
-                "pref_location" -> {
+                PREF_KEY_LOCATION -> {
                     val locationValue = arrLocationKey[arrLocationValue.indexOf(sharedPreferences.getString(key, "seoul"))]
                     updateCostInfo(locationValue)
                     updateSummary(key, locationValue)
-                    Log.d("PREFS", sharedPreferences.getString(key, "seoul").toString())
                 }
 
-                "pref_theme" -> {
+                PREF_KEY_THEME -> {
                     updateSummary(key, arrThemeKey[arrThemeValue.indexOf(sharedPreferences.getString(key, "horse"))])
-                    Log.d("PREFS", sharedPreferences.getString(key, "horse").toString())
                 }
             }
         }
 
     private fun initSummary() {
-        val locationValue = arrLocationKey[arrLocationValue.indexOf(pref.getString("pref_location", "seoul"))]
+        val locationValue = arrLocationKey[arrLocationValue.indexOf(pref.getString(PREF_KEY_LOCATION, "seoul"))]
         updateCostInfo(locationValue)
-        updateSummary("pref_location", locationValue)
-        updateSummary("pref_theme", arrThemeKey[arrThemeValue.indexOf(pref.getString("pref_theme", "horse"))])
-        updateSummary("pref_info_version", pref.getString("pref_info_version", "20001022")!!)
+        updateSummary(PREF_KEY_LOCATION, locationValue)
+        updateSummary(PREF_KEY_THEME, arrThemeKey[arrThemeValue.indexOf(pref.getString(PREF_KEY_THEME, "horse"))])
+        updateSummary(PREF_KEY_INFO_VERSION, pref.getString(PREF_KEY_INFO_VERSION, "20001022")!!)
     }
 
     private fun updateCostInfo(location: String) {
-        updateSummary("pref_info_cost", "$location COST_INFO")
+        updateSummary(PREF_KEY_INFO_COST, "$location COST_INFO")
     }
 
     private fun updateSummary(key: String, data: String) {
