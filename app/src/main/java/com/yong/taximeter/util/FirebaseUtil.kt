0ce8f02a5413cb5b5fbc488b88ef9e96e0b60b6f
yymin1022 <yymin1022@gmail.com>
2023-07-22 00:01:15 +0900

    Update FirebaseUtil : Added Function for Applying Latest Cost DB

--- app/src/main/java/com/yong/taximeter/util/FirebaseUtil.kt
+++ app/src/main/java/com/yong/taximeter/util/FirebaseUtil.kt
@@ -12,7 +12,7 @@ object FirebaseUtil {
     fun getLatestVersion(context: Context) {
         val pref = PreferenceManager.getDefaultSharedPreferences(context)
         var chkVersion: String
-        var curVersion = pref.getString("pref_info_version", "00000000")
+        val curVersion = pref.getString("pref_info_version", "00000000")
 
         val db = Firebase.firestore
         val docRef = db.collection("cost").document("version")
@@ -21,8 +21,35 @@ object FirebaseUtil {
                 chkVersion = document.get("data").toString()
 
                 if(curVersion!! < chkVersion) {
-                    Toast.makeText(context, context.resources.getString(R.string.firebase_toast_update), Toast.LENGTH_SHORT).show()
+                    updateCostInfo(context)
                 }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, context.resources.getString(R.string.firebase_toast_fail), Toast.LENGTH_SHORT).show()
+                Log.e("VerCheck", exception.message.toString())
+            }
+    }
+
+    fun updateCostInfo(context: Context) {
+        val db = Firebase.firestore
+        val docRef = db.collection("cost").document("info")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                for(costInfo in (document.get("data") as List<*>)) {
+                    val costInfoItem = costInfo as Map<*, *>
+                    val costInfoCity = costInfoItem["city"].toString()
+                    val costInfoData = costInfoItem["data"] as Map<*, *>
+
+                    val pref = context.getSharedPreferences("prefs_cost_${costInfoCity}", Context.MODE_PRIVATE)
+                    val prefEditor = pref.edit()
+
+                    costInfoData.forEach {
+                        prefEditor.putInt(it.key.toString(), (it.value as Long).toInt())
+                    }
+                    prefEditor.apply()
+                }
+
+                Toast.makeText(context, context.resources.getString(R.string.firebase_toast_update), Toast.LENGTH_SHORT).show()
             }
             .addOnFailureListener { exception ->
                 Toast.makeText(context, context.resources.getString(R.string.firebase_toast_fail), Toast.LENGTH_SHORT).show()
