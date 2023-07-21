8619b1a72e1b688b7e2ef9f5f49e57b400cc7f92
yymin1022 <yymin1022@gmail.com>
2023-07-21 23:11:20 +0900

    Update FirebaseUtil : Added Function for Check Latest Version

--- app/src/main/java/com/yong/taximeter/util/FirebaseUtil.kt
+++ app/src/main/java/com/yong/taximeter/util/FirebaseUtil.kt
@@ -1,4 +1,32 @@
 package com.yong.taximeter.util
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.preference.PreferenceManager
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.yong.taximeter.R
+
 object FirebaseUtil {
+    fun getLatestVersion(context: Context) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(context)
+        var chkVersion: String
+        var curVersion = pref.getString("pref_info_version", "00000000")
+
+        val db = Firebase.firestore
+        val docRef = db.collection("cost").document("version")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                chkVersion = document.get("data").toString()
+
+                if(curVersion!! < chkVersion) {
+                    Toast.makeText(context, context.resources.getString(R.string.firebase_toast_update), Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, context.resources.getString(R.string.firebase_toast_fail), Toast.LENGTH_SHORT).show()
+                Log.e("VerCheck", exception.message.toString())
+            }
+    }
 }
