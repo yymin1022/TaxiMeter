f5b8ff2cd600866b3fc3959523ea739ad59d56e8
yymin1022 <yymin1022@gmail.com>
2023-07-23 13:46:10 +0900

    Added MeterService

---
+++ app/src/main/java/com/yong/taximeter/service/MeterService.kt
@@ -0,0 +1,11 @@
+package com.yong.taximeter.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MeterService: Service {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
