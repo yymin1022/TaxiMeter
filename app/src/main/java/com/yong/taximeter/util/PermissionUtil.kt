a7835bc415b1f271d0e85c81b94226d32712fcf4
yymin1022 <yymin1022@gmail.com>
2023-07-23 17:48:41 +0900

    Update PermissionUtil : Cleaned UP

--- app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
+++ app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
@@ -8,7 +8,6 @@ import android.net.Uri
 import android.provider.Settings
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import com.google.firestore.v1.TargetOrBuilder
 import com.yong.taximeter.R
 
 object PermissionUtil {
