12ee67d18a6ea3838c92336b97edff2feed767db
yymin1022 <yymin1022@gmail.com>
2023-07-23 17:36:53 +0900

    Update MeterService : Applied PermissionUtil

--- app/src/main/java/com/yong/taximeter/service/MeterService.kt
+++ app/src/main/java/com/yong/taximeter/service/MeterService.kt
@@ -1,25 +1,22 @@
 package com.yong.taximeter.service
 
-import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
 import android.os.IBinder
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.yong.taximeter.R
 import com.yong.taximeter.activity.MeterActivity
 import com.yong.taximeter.util.MeterUtil
+import com.yong.taximeter.util.PermissionUtil
 import kotlin.math.roundToInt
 
 class MeterService: Service(), LocationListener {
@@ -27,12 +24,8 @@ class MeterService: Service(), LocationListener {
     private lateinit var notificationBuilder: NotificationCompat.Builder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(this, "Permission Error", Toast.LENGTH_SHORT).show()
-            return super.onStartCommand(intent, flags, startId)
+        if(!PermissionUtil.checkPermission(this)){
+            stopSelf()
         }
 
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
