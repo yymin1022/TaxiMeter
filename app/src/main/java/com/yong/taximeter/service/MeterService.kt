d67907dfa331ca0775e8998c9f0c2c22d9c75174
yymin1022 <yymin1022@gmail.com>
2023-07-23 14:09:08 +0900

    Update MeterService : Added Base Functions

--- app/src/main/java/com/yong/taximeter/service/MeterService.kt
+++ app/src/main/java/com/yong/taximeter/service/MeterService.kt
@@ -4,8 +4,16 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 
-class MeterService: Service {
+class MeterService: Service() {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+        return null
     }
 }
