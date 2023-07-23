f9b78630e6e894347b3e575706e75ed141f181d1
yymin1022 <yymin1022@gmail.com>
2023-07-23 17:34:02 +0900

    Update PermissionUtil : Added Function for Permission Checking

--- app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
+++ app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
@@ -1,4 +1,18 @@
 package com.yong.taximeter.util
 
-class PermissionUtil {
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
+object PermissionUtil {
+    fun checkPermission(context: Context): Boolean {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            return false
+        }
+        return true
+    }
 }
