dbf436576c4f8ad852376d7aa231cdba470a994b
yymin1022 <yymin1022@gmail.com>
2023-07-23 17:48:28 +0900

    Update PermissionUtil : Added Function for Open App Info

--- app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
+++ app/src/main/java/com/yong/taximeter/util/PermissionUtil.kt
@@ -2,8 +2,14 @@ package com.yong.taximeter.util
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.provider.Settings
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.firestore.v1.TargetOrBuilder
+import com.yong.taximeter.R
 
 object PermissionUtil {
     fun checkPermission(context: Context): Boolean {
@@ -14,5 +20,15 @@ object PermissionUtil {
             return false
         }
         return true
+    }
+
+    fun openAppInfo(context: Context) {
+        Toast.makeText(context, context.getString(R.string.noti_toast_no_permission), Toast.LENGTH_LONG).show()
+        val permissionIntent = Intent().apply {
+            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            addCategory(Intent.CATEGORY_DEFAULT)
+            data = Uri.parse("package:com.yong.taximeter")
+        }
+        context.startActivity(permissionIntent)
     }
 }
