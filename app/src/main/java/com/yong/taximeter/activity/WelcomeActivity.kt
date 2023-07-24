0f620f70ba1ceedc88fb7f3b8c9dbb8aba5a05cf
yymin1022 <yymin1022@gmail.com>
2023-07-24 21:21:18 +0900

    Update WelcomeActivity : Added Fragments

--- app/src/main/java/com/yong/taximeter/activity/WelcomeActivity.kt
+++ app/src/main/java/com/yong/taximeter/activity/WelcomeActivity.kt
@@ -3,10 +3,19 @@ package com.yong.taximeter.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yong.taximeter.R
+import com.yong.taximeter.fragment.WelcomeLocationFragment
+import com.yong.taximeter.fragment.WelcomePermissonFragment
 
 class WelcomeActivity : AppCompatActivity() {
+    private var welcomeFragmentManager = supportFragmentManager
+    private var welcomeLocationFragment = WelcomeLocationFragment()
+    private var welcomePermissonFragment = WelcomePermissonFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val welcomeFragmentTransaction = welcomeFragmentManager.beginTransaction()
+        welcomeFragmentTransaction.replace(R.id.layout_welcome_fragment, welcomePermissonFragment).commitAllowingStateLoss()
     }
 }
