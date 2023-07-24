d41520dfa1a8277f8ca9c1a77eeaef8ed41ea1e0
yymin1022 <yymin1022@gmail.com>
2023-07-24 20:58:48 +0900

    Added WelcomeActivity

---
+++ app/src/main/java/com/yong/taximeter/activity/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.yong.taximeter.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yong.taximeter.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
