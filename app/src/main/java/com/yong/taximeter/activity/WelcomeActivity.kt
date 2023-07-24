c38a7282a8a2d09ebe68f55b7ff92cacc9183bb2
yymin1022 <yymin1022@gmail.com>
2023-07-25 00:42:23 +0900

    Update WelcomeActivity : Disabled Back Button

--- app/src/main/java/com/yong/taximeter/activity/WelcomeActivity.kt
+++ app/src/main/java/com/yong/taximeter/activity/WelcomeActivity.kt
@@ -2,6 +2,7 @@ package com.yong.taximeter.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import com.yong.taximeter.R
 import com.yong.taximeter.fragment.WelcomePermissonFragment
 
@@ -13,7 +14,13 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
+        this.onBackPressedDispatcher.addCallback(this, backButtonCallback)
+
         val welcomeFragmentTransaction = welcomeFragmentManager.beginTransaction()
         welcomeFragmentTransaction.replace(R.id.layout_welcome_fragment, welcomePermissonFragment).commitAllowingStateLoss()
+    }
+
+    private val backButtonCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {}
     }
 }
